f68203beed33f9b384b09fe69626c54701ca2698
Ming-Yu Liu <mingyu.liu.tw@gmail.com>
2018-02-22 13:26:53 -0800

    add demo.sh

---
+++ demo.sh
@@ -0,0 +1,8 @@
+mkdir images && mkdir results;
+cd images;
+axel -n 1 http://freebigpictures.com/wp-content/uploads/shady-forest.jpg --output=content1.png;
+axel -n 1 https://vignette.wikia.nocookie.net/strangerthings8338/images/e/e0/Wiki-background.jpeg/revision/latest?cb=20170522192233 --output=style1.png;
+convert -resize 25% content1.png content1.png;
+convert -resize 50% style1.png style1.png;
+cd ..;
+python demo.py;
