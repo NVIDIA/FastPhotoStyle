d5db65ebe294d90fda1ae2a74fb3a4218193f06f
suquark <zsy9509@mail.ustc.edu.cn>
2018-02-24 22:42:23 +0800

    Add the submodule for model files. Fix demos.

--- demo.py
+++ demo.py
@@ -5,20 +5,13 @@ Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses
 
 import argparse
 
+import torch
+
 import process_stylization
 from photo_wct import PhotoWCT
 
 parser = argparse.ArgumentParser(description='Photorealistic Image Stylization')
-parser.add_argument('--vgg1', default='./models/vgg_normalised_conv1_1_mask.t7', help='Path to the VGG conv1_1')
-parser.add_argument('--vgg2', default='./models/vgg_normalised_conv2_1_mask.t7', help='Path to the VGG conv2_1')
-parser.add_argument('--vgg3', default='./models/vgg_normalised_conv3_1_mask.t7', help='Path to the VGG conv3_1')
-parser.add_argument('--vgg4', default='./models/vgg_normalised_conv4_1_mask.t7', help='Path to the VGG conv4_1')
-parser.add_argument('--vgg5', default='./models/vgg_normalised_conv5_1_mask.t7', help='Path to the VGG conv5_1')
-parser.add_argument('--decoder5', default='./models/feature_invertor_conv5_1_mask.t7', help='Path to the decoder5')
-parser.add_argument('--decoder4', default='./models/feature_invertor_conv4_1_mask.t7', help='Path to the decoder4')
-parser.add_argument('--decoder3', default='./models/feature_invertor_conv3_1_mask.t7', help='Path to the decoder3')
-parser.add_argument('--decoder2', default='./models/feature_invertor_conv2_1_mask.t7', help='Path to the decoder2')
-parser.add_argument('--decoder1', default='./models/feature_invertor_conv1_1_mask.t7', help='Path to the decoder1')
+parser.add_argument('--model', default='./PhotoWCTModels/photo_wct.pth', help='Path to the PhotoWCT model')
 parser.add_argument('--content_image_path', default='./images/content1.png')
 parser.add_argument('--content_seg_path', default=[])
 parser.add_argument('--style_image_path', default='./images/style1.png')
@@ -27,7 +20,8 @@ parser.add_argument('--output_image_path', default='./results/example1.png')
 args = parser.parse_args()
 
 # Load model
-p_wct = PhotoWCT(args)
+p_wct = PhotoWCT()
+p_wct.load_state_dict(torch.load(args.model))
 p_wct.cuda(0)
 
 process_stylization.stylization(
