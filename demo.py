bed87fb5f5f9d8f0552be3fc7be478e9db27d2a0
suquark <zsy9509@mail.ustc.edu.cn>
2018-02-25 16:26:12 +0800

    More info about model loading & converting

--- demo.py
+++ demo.py
@@ -3,6 +3,7 @@ Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 """
 
+from __future__ import print_function
 import argparse
 
 import torch
@@ -11,7 +12,8 @@ import process_stylization
 from photo_wct import PhotoWCT
 
 parser = argparse.ArgumentParser(description='Photorealistic Image Stylization')
-parser.add_argument('--model', default='./PhotoWCTModels/photo_wct.pth', help='Path to the PhotoWCT model')
+parser.add_argument('--model', default='./PhotoWCTModels/photo_wct.pth',
+                    help='Path to the PhotoWCT model. These are provided by the PhotoWCT submodule, please use `git submodule update --init --recursive` to pull.')
 parser.add_argument('--content_image_path', default='./images/content1.png')
 parser.add_argument('--content_seg_path', default=[])
 parser.add_argument('--style_image_path', default='./images/style1.png')
@@ -21,7 +23,12 @@ args = parser.parse_args()
 
 # Load model
 p_wct = PhotoWCT()
-p_wct.load_state_dict(torch.load(args.model))
+try:
+    p_wct.load_state_dict(torch.load(args.model))
+except:
+    print("Fail to load PhotoWCT models. PhotoWCT submodule not updated?")
+    exit()
+    
 p_wct.cuda(0)
 
 process_stylization.stylization(
